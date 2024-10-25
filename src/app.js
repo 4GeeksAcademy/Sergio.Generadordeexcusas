676fe080c4a83149125eff8f76238d98a1ac8383
Sergio Brenes <brenes93@hotmail.es>
2024-10-25 08:22:52 +0000

    Primer commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    
+  let primero = "Un ";
+  let objeto = ["Perro ", "Gato ", "Samurai ", "Alien "];
+  let accion = ["se llevó ", "robó ", "pisó ", "mordió "];
+  let segundo = "mi ";
+  let propiedad = ["teléfono ", "ordenador ", "tabaco ", "cartera "];
+  let lugar = ["en la piscina.", "en el trabajo.", "en la puerta de la discoteca.", "en el gimnasio."];
+    
+      
+    
+      
+  let rdm1 = Math.floor(Math.random() * objeto.length);
+  let rdm2 = Math.floor(Math.random() * accion.length);
+  let rdm3 = Math.floor(Math.random() * propiedad.length);
+  let rdm4 = Math.floor(Math.random() * lugar.length);
+    
+      
+   
+  document.querySelector("#excuse").innerHTML = primero + objeto[rdm1] + accion[rdm2] + segundo + propiedad[rdm3] + lugar[rdm4];
+  };
